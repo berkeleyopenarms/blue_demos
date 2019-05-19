5db4ab6c18a820609fd32184540d827f5b257b16
Brent Yi <yibrenth@gmail.com>
2019-05-19 01:17:40 -0700

    add deadman

--- spacemouse_teleop/spacemouse_teleop.py
+++ spacemouse_teleop/spacemouse_teleop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from blue_interface import BlueInterface
+from blue_interface import BlueInterface, _BlueControlMode
 import numpy as np
 import time
 import transformations as t
@@ -39,55 +39,60 @@ if __name__ == '__main__':
     gripper_closed = False
 
     while True:
-        ## Read current state
-        current_pose = blue.get_cartesian_pose()
-        current_position = current_pose["position"]
-        current_orientation = current_pose["orientation"]
+        if mouse.input_button0:
+            blue._set_control_mode(_BlueControlMode.POSITION)
 
-        ## Compute new target pose
-        if np.linalg.norm(mouse.input_pos) > 10:
-            input_pos = np.asarray([
-                mouse.input_pos[1],
-                mouse.input_pos[0],
-                -mouse.input_pos[2]
-            ]) / 1000.0
-            target_position = current_position + input_pos
+            ## Read current state
+            current_pose = blue.get_cartesian_pose()
+            current_position = current_pose["position"]
+            current_orientation = current_pose["orientation"]
 
-        if np.linalg.norm(mouse.input_rot) > 10:
-            input_rot = t.quaternion_from_euler(*[
-                mouse.input_rot[1] / 200.0,
-                mouse.input_rot[0] / 200.0,
-                -mouse.input_rot[2] / 150.0
-            ])
-            target_orientation = t.quaternion_multiply(input_rot, current_orientation)
+            ## Compute new target pose
+            if np.linalg.norm(mouse.input_pos) > 10:
+                input_pos = np.asarray([
+                    mouse.input_pos[1],
+                    mouse.input_pos[0],
+                    -mouse.input_pos[2]
+                ]) / 1000.0
+                target_position = current_position + input_pos
 
-        ## Publishing target pose
-        pose_target_msg = {
-            "header": {
-                "frame_id": "base_link"
-            },
-            "pose": {
-                "position": dict(zip(
-                    ("x", "y", "z"),
-                    target_position
-                )),
-                "orientation": dict(zip(
-                    ("x", "y", "z", "w"),
-                    target_orientation
-                ))
+            if np.linalg.norm(mouse.input_rot) > 10:
+                input_rot = t.quaternion_from_euler(*[
+                    mouse.input_rot[1] / 200.0,
+                    mouse.input_rot[0] / 200.0,
+                    -mouse.input_rot[2] / 150.0
+                ])
+                target_orientation = t.quaternion_multiply(input_rot, current_orientation)
+
+            ## Publishing target pose
+            pose_target_msg = {
+                "header": {
+                    "frame_id": "base_link"
+                },
+                "pose": {
+                    "position": dict(zip(
+                        ("x", "y", "z"),
+                        target_position
+                    )),
+                    "orientation": dict(zip(
+                        ("x", "y", "z", "w"),
+                        target_orientation
+                    ))
+                }
             }
-        }
-        pose_target_publisher.publish(pose_target_msg)
+            pose_target_publisher.publish(pose_target_msg)
 
-        ## Gripper stuff
-        if gripper_closed != mouse.input_button1:
-            if gripper_closed:
-                # open gripper
-                blue.command_gripper(0.0, 10.0)
-            else:
-                # close gripper
-                blue.command_gripper(-1.5, 20.0)
-            gripper_closed = mouse.input_button1
+            ## Gripper stuff
+            if gripper_closed != mouse.input_button1:
+                if gripper_closed:
+                    # open gripper
+                    blue.command_gripper(0.0, 10.0)
+                else:
+                    # close gripper
+                    blue.command_gripper(-1.5, 20.0)
+                gripper_closed = mouse.input_button1
+        else:
+            blue.disable_control()
 
         ## Sleep
         time.sleep(0.01)
