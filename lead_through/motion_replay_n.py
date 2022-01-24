30a18360c5ff90f1a2a1f691734c6d2a53609eb0
Philipp <phil80301@berkeley.edu>
2022-01-23 21:16:50 -0800

    motions

--- lead_through/motion_replay_n.py
+++ lead_through/motion_replay_n.py
@@ -17,8 +17,13 @@ if __name__ == '__main__':
     # Blue Python interface
     arm_side = "right"
     address = "127.0.0.1"
-    blue = BlueInterface(arm_side, address)
-    blue.disable_control()
+
+    blue0 = BlueInterface(arm_side, address, port="9090")
+    blue0.disable_control()
+    blue1 = BlueInterface(arm_side, address, port="9091")
+    blue1.disable_control()
+    blue2 = BlueInterface(arm_side, address, port="9092")
+    blue2.disable_control()
 
     data = pickle.load( open(filename, "rb")) #uses the pickle function to read the binary file created in record_poses.py
     joint_angle_list, _, gripper_list, frequency = data
@@ -30,12 +35,17 @@ if __name__ == '__main__':
             last_time = 0.0
             for i in range (len(joint_angle_list)):
                 #if len(joint_angle_list[i]) == 7:
-                blue.set_joint_positions(np.array(joint_angle_list[i])) # tell the robot to go to a set of joint angles
-                #blue.command_gripper(gripper_list[i], 30.0)
+                blue0.set_joint_positions(np.array(joint_angle_list[i])) # tell the robot to go to a set of joint angles
+                blue1.set_joint_positions(np.array(joint_angle_list[i])) # tell the robot to go to a set of joint angles
+                blue2.set_joint_positions(np.array(joint_angle_list[i])) # tell the robot to go to a set of joint angles
                 if gripper_list[i] < -0.2:
-                    blue.command_gripper(-1.3, 15.0)
+                    blue0.command_gripper(-1.3, 15.0)
+                    blue1.command_gripper(-1.3, 15.0)
+                    blue2.command_gripper(-1.3, 15.0)
                 else:
-                    blue.command_gripper(0, 2.0)
+                    blue0.command_gripper(0, 2.0)
+                    blue1.command_gripper(0, 2.0)
+                    blue2.command_gripper(0, 2.0)
                 sleep_time = 1.0/frequency - (time.time() - last_time)
                 if sleep_time > 0:
                     time.sleep(sleep_time)
